19c2a05cc92c8f06af6112b3824c89fc3b6c267e
user1 <user1@DESKTOP-JDCC25S>
2024-11-15 12:43:03 +0900

    [ADD] 24/11/15 입력스캐너 정리
    1. 스캐너에 대해서 정리함
    -Java API이면서 import를 해서 사용함.
    2. 다양한 명령어를 통해서 int, double, 문자영 등을 입력받을 수 있음.

---
+++ src/com/kh/java/inputsc/Excam_Scanner1.java
@@ -0,0 +1,20 @@
+package com.kh.java.inputsc;
+
+import java.util.Scanner;
+
+public class Excam_Scanner1 {
+	public static void main(String[]args) {
+		/*
+		 * Scanner : Java API, 이미 만들어진 클래스
+		 * 키보드를 통해 들어온 입력값을 사용할 수 있도록 해 줌
+		 * 사용법 : Scanner 변수명 = new Scanner(System.in);
+		 * 변수명 외에는 바뀔 수 없음, 그대로 적어야 함.
+		 * Scanner를 썼을 때 반드시 import(ctrl+space bar)를 
+		 * 해주어야 함.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+	}
+	
+}
